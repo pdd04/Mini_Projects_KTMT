130d7b01e1fb5d125b28004225a15a2feb936cce
Long-1911 <longpham19112004@gmail.com>
2024-03-27 22:06:54 +0700

    first commit

--- Phan_A/Cau9.asm
+++ Phan_A/Cau9.asm
@@ -1,6 +1,6 @@
 .data
 	Message: .asciiz "Nhap vao 1 so nguyen duong N: "
-	Message1: .asciiz "Chu so lon nhat cua N la: "
+	Message1: .asciiz "Chu so nho nhat cua N la: "
 	Eror: .asciiz "So nhap vao phai lon hon 0!\n"
 	Eror1: .asciiz "So nhap vao phai co it nhat 2 chu so!\n"
 .text
@@ -19,9 +19,9 @@ main:
 	# Kiểm tra N có ít nhất 2 chữ số không
 	li $t0, 10
 	blt $s0, $t0, PrintEror2
-	# Khởi tạo biến $s1 để lưu giá trị chữ số lớn nhất, ban đầu gán bằng 0
-	li $s1, 0
-	# Gọi hàm check để tìm chữ số lớn nhất
+	# Khởi tạo biến $s1 để lưu giá trị chữ số nhỏ nhất, ban đầu gán bằng 9
+	li $s1, 9
+	# Gọi hàm check để tìm chữ số nhỏ nhất
 	j check
 	
 exit:
@@ -33,18 +33,18 @@ check:
 	# Kiểm tra N có bằng 0 không
 	beqz $s0, PrintResult
 	# Chia N cho 10 để lấy chữ số cuối cùng và phần dư
-	div $s0, $t0
+	div  $s0, $t0
 	mfhi $t1
 	mflo $t2
 	# Lấy chữ số cuối cùng và cập nhật giá trị của N
 	addi $s0, $t2, 0
-	# So sánh chữ số hiện tại với chữ số lớn nhất hiện tại
-	bgt $t1, $s1, Update 
+	# So sánh chữ số hiện tại với chữ số nhỏ nhất hiện tại
+	blt  $t1, $s1, Update  
 	# Nếu chữ số hiện tại không lớn hơn chữ số lớn nhất, tiếp tục kiểm tra các chữ số khác
 	j check
 	
 Update:
-	# Nếu chữ số hiện tại lớn hơn chữ số lớn nhất, cập nhật lại giá trị của chữ số lớn nhất
+	# Nếu chữ số hiện tại nhỏ hơn chữ số nhỏ nhất, cập nhật lại giá trị của chữ số nhỏ nhất
 	addi $s1, $t1, 0
 	# Tiếp tục kiểm tra các chữ số khác
 	j check
@@ -66,11 +66,11 @@ PrintEror2:
 	j main
 	
 PrintResult:
-	# In thông báo chữ số lớn nhất của N
+	# In thông báo chữ số nhỏ nhất của N
 	li $v0, 4
 	la $a0, Message1
 	syscall
-	# In chữ số lớn nhất
+	# In chữ số nhỏ nhất
 	li $v0, 1
 	addi $a0, $s1, 0
 	syscall
