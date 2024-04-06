a253f3e62c3bc4ed014e515dd49000e8afde4ca6
Long-1911 <longpham19112004@gmail.com>
2024-04-06 13:30:10 +0700

    Nhập mảng số nguyên từ bàn phím. In ra màn hình số chẵn lớn nhất nhỏ hơn mọi số lẻ trong mảng.

--- Phan_B/Cau18.asm
+++ Phan_B/Cau18.asm
@@ -4,8 +4,6 @@
 	Message1: 	.asciiz "Nhap so: "
 	Message2: 	.asciiz "So chan lon nhat nho hon moi so le trong mang la: "
 	Eror: 		.asciiz "So phan tu mang phai lon hon bang 1!\n"
-	Eror1:		.asciiz "Mang khong co so le!\n"
-	Eror2:		.asciiz "Mang khong co so chan hoac khong tim duoc so chan thoa man de bai!\n"
 	
 .text
 main:
@@ -50,7 +48,7 @@ init:
 	li	$s7, 999999	# $s7 = minOdd = gia tri le be nhat la 999999
 	
 CheckOdd:	
-	beq	$s2, $s0, CheckEror1	# i = N => CheckMaxEven branch
+	beq	$s2, $s0, init2		# i = N => CheckMaxEven branch
 	lw	$t8, 0($a2)		# $t8 = A[i]
 	div	$t8, $t9		# A[i] % 2
 	mfhi	$t7			# Lay mode cua phep chia tren
@@ -62,18 +60,12 @@ CheckOdd:
 CheckMinOdd:
 	slt	$t6, $t8, $s7		# Neu A[i] < minOdd => $t6 = 1
 	beq	$t6, 1, UpdateMinOdd	# Neu thoa man thi gan gia tri minOdd = A[i]
-	addi	$a2, $a2, 4		# address of next index
-	addi	$s2, $s2, 1		# i++
-	j	CheckOdd
-	
+
 UpdateMinOdd:
 	addi	$s7, $t8, 0		# minOdd = A[i]
 	addi	$a2, $a2, 4		# address of next index
 	addi	$s2, $s2, 1		# i++
 	j	CheckOdd
-
-CheckEror1:
-	beq	$s7, 999999, PrintEror1		# Neu khong tim duoc so le thi error
 	
 # -----------------------------------------------------------------------------
 # Sau khi tim duoc so le nho nhat, ta di tim so chan lon nhat nho hon so le do!
@@ -81,10 +73,10 @@ init2:
 	addi 	$a2, $a1, 0	# $a2 = $a1 = address of A[0]
 	li	$s2, 0		# $s2 = i = 0
 	li	$t9, 2		# $t9 = 2
-	li	$s6, -1		# $s6 = maxEven = gia tri chan lonw nhat la -1
+	li	$s6, 0		# $s6 = maxEven = gia tri chan lonw nhat la 0
 
 CheckEven:	
-	beq	$s2, $s0, CheckEror2 	# i = N => CheckMaxEven branch
+	beq	$s2, $s0, Print 	# i = N => CheckMaxEven branch
 	lw	$t8, 0($a2)		# $t8 = A[i]
 	div	$t8, $t9		# A[i] % 2
 	mfhi	$t7			# Lay mode cua phep chia tren
@@ -98,18 +90,12 @@ CheckMaxEven:
 	slt	$t5, $t8, $s7		# Neu A[i] < minOdd => $t5 = 1
 	add	$t4, $t5, $t6		
 	beq	$t4, 2, UpdateMaxEven	# Neu thoa man hai dieu kien tren thi gan maxEven = A[i]
-	addi	$a2, $a2, 4		# address of next index
-	addi	$s2, $s2, 1		# i++
-	j	CheckEven
 
 UpdateMaxEven:
 	addi	$s6, $t8, 0		# maxEven = A[i]
 	addi	$a2, $a2, 4		# address of next index
 	addi	$s2, $s2, 1		# i++
 	j	CheckEven
-
-CheckEror2:
-	beq	$s6, -1, PrintEror2	# Neu khong tim duoc so chan thi error
 
 Print:
 	# In ra ket qua
@@ -126,25 +112,4 @@ PrintEror:
 	li 	$v0, 4
 	la 	$a0, Eror
 	syscall
-	
-	li	$t1, 0
-	li	$t2, 0
-	j 	main
-	
-PrintEror1:
-	li 	$v0, 4
-	la 	$a0, Eror1
-	syscall
-	
-	li	$t1, 0
-	li	$t2, 0
-	j 	main
-
-PrintEror2:
-	li 	$v0, 4
-	la 	$a0, Eror2
-	syscall
-	
-	li	$t1, 0
-	li	$t2, 0
 	j 	main
